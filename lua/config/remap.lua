73a54daafc72b8865e4e39c05efdc7a35b3eb6c5
lyh <liuyuhao0414@gmail.com>
2024-09-14 22:15:52 +0800

    packer -> lazy

---
+++ lua/config/remap.lua
@@ -0,0 +1,21 @@
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<leader>bx", ":bw<CR>")
+vim.keymap.set("n", "<leader>ka", ":bufdo bd<CR>")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
+
+vim.keymap.set("n", "<C-s>", ":w!<CR>")
+vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>ca", ":lua vim.lsp.buf.code_action()<CR>")
+
+vim.keymap.set("n", "<leader>ud", vim.cmd.UndotreeToggle)
+
