57f9a3ab3164ac11365aa8dbe71b825b4e1f267a
lyh <liuyuhao0414@gmail.com>
2024-10-13 06:35:27 +0800

    add leap

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,39 @@
 return {
-    "bluz71/vim-moonfly-colors",
-    name = "moonfly",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.g.moonflyTransparent = true
-        vim.cmd([[colorscheme moonfly]])
-    end,
+        local tokyonight = require("tokyonight")
+
+        tokyonight.setup({
+            transparent = true,
+            style = "storm",
+        })
+
+        vim.cmd.colorscheme("tokyonight")
+    end
 }
+-- return {
+--     "oxfist/night-owl.nvim",
+--     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+--     priority = 1000, -- make sure to load this before all the other start plugins
+--     config = function()
+--         -- load the colorscheme here
+--         require("night-owl").setup()
+--         vim.cmd.colorscheme("night-owl")
+--     end,
+-- }
+
+-- return {
+--     "bluz71/vim-moonfly-colors",
+--     name = "moonfly",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         vim.g.moonflyTranspaeent = true
+--         vim.cmd([[colorscheme moonfly]])
+--     end,
+-- }
 
 -- return {
 --     "catppuccin/nvim",
