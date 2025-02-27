7b83ae8c05d898bb1ca2ae161cd60b052aea682a
lyh <liuyuhao0414@gmail.com>
2025-02-27 23:57:56 +0800

    update

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,17 +1,22 @@
-return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    config = function()
-        local highlight = { "GRAY" }
+if vim.g.vscode
+then 
+    return {}
+else
+    return {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        config = function()
+            local highlight = { "GRAY" }
 
-        local hooks = require "ibl.hooks"
-        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-            vim.api.nvim_set_hl(0, "GRAY", { fg = "#525975" })
-        end)
+            local hooks = require "ibl.hooks"
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "GRAY", { fg = "#525975" })
+            end)
 
-        require("ibl").setup {
-            indent = { char = "▏", highlight = highlight },
-            scope = { enabled = false }
-        }
-    end
-}
+            require("ibl").setup {
+                indent = { char = "▏", highlight = highlight },
+                scope = { enabled = false }
+            }
+        end
+    }
+end
